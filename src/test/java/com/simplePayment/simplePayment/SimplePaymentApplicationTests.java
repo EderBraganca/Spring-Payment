d8cc8f50df8849ea92532e689547ed560f3d3d05
EderBraganca <88894912+EderBraganca@users.noreply.github.com>
2024-07-18 19:30:26 -0300

    Spring Initializer

---
+++ src/test/java/com/simplePayment/simplePayment/SimplePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplePayment.simplePayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

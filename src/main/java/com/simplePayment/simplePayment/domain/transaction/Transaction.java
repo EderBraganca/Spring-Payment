bbdff7a6d103844758e0945e0da900e2128f28a7
EderBraganca <88894912+EderBraganca@users.noreply.github.com>
2024-07-18 20:01:45 -0300

    Create Domains.2

---
+++ src/main/java/com/simplePayment/simplePayment/domain/transaction/Transaction.java
@@ -0,0 +1,27 @@
+package com.simplePayment.simplePayment.domain.transaction;
+
+import com.simplePayment.simplePayment.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+}

2432396e82bc3d6383b2cafc0ec3348844c84ae1
EderBraganca <88894912+EderBraganca@users.noreply.github.com>
2024-07-18 23:15:33 -0300

    Domains Updates

--- src/main/java/com/simplePayment/simplePayment/domain/transaction/Transaction.java
+++ src/main/java/com/simplePayment/simplePayment/domain/transaction/Transaction.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity(name="transactions")
 @Table(name="transactions")
@@ -24,4 +25,5 @@ public class Transaction {
     @ManyToOne
     @JoinColumn(name = "receiver_id")
     private User receiver;
+    private LocalDateTime timestamp;
 }
--- src/main/java/com/simplePayment/simplePayment/domain/user/User.java
+++ src/main/java/com/simplePayment/simplePayment/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.simplePayment.simplePayment.domain.user;
 
+import com.simplePayment.simplePayment.dtos.UserDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -26,4 +27,14 @@ public class User {
     private BigDecimal balance;
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+
+    public User(UserDTO data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.email = data.email();
+        this.password = data.password();
+    }
 }
